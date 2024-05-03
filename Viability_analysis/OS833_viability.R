0c16666a5d265e54587e35c9d22525a5226016da
Brenda Melano <brendamelano@Brendas-iMac.local>
2024-05-02 17:18:30 -0700

    nfe2l3 viability

--- Viability_analysis/OS833_viability.R
+++ Viability_analysis/OS833_viability.R
@@ -8,6 +8,75 @@ library(dplyr)
 library(ggplot2)
 library(tidyverse)
 
+
+######    NFE2L3   ######
+
+
+
+# Reading in the data
+OS833_NFE2L3_viability <- read.csv("~/Desktop/Reprogramming_Osteosarcoma/Viability_analysis/Processed_2024_04_23_833_NFE2L3_viability_15_day.csv")
+
+
+pastel_colors <- c("ctrl" = "#FFB3BA", "nfe2l3" = "#A6E1FA")
+
+OS833_NFE2L3_viability <- OS833_NFE2L3_viability %>%
+  filter(drug %in% c("atr", "ctrl"), gene %in% c("ctrl", "nfe2l3"))
+
+# Computing the mean for each gene/drug pair
+summary_df <- OS833_NFE2L3_viability %>%
+  group_by(drug, gene) %>%
+  summarize(
+    mean_value = mean(value, na.rm = TRUE),
+    sd_value = sd(value, na.rm = TRUE)
+  )
+
+# Assuming your data frame OS833_NFE2L3_viability has a column 'value' for the measurement
+t_test_results <- t.test(value ~ gene,
+                         data = filter(OS833_NFE2L3_viability, drug == "atr"),
+                         var.equal = TRUE)  # assuming equal variances for simplicity
+p_value <- t_test_results$p.value  # Extracting the p-value from the test results
+
+
+# 
+OS833_NFE2L3 <- ggplot(summary_df, aes(x=drug, y=mean_value, fill=gene)) +
+  geom_bar(stat="identity", position="dodge") +
+  geom_errorbar(aes(ymin=mean_value-sd_value, ymax=mean_value+sd_value), 
+                width=.2,                    # Adjust the width of the error bars
+                position=position_dodge(.9)) +
+  labs(title="In vitro Atr effiacy with NFE2L3 KD", 
+       y="Fluorescent Measurement", x="Gene") +
+  theme_bw() +  # Use theme_bw as a base theme
+  theme(panel.grid.major = element_blank(),  # Remove major grid lines
+        panel.grid.minor = element_blank()) +  # Remove minor grid lines
+  scale_fill_manual(values = pastel_colors) +
+  # Annotate with the p-value
+  annotate("text", x = 1, y = max(summary_df$mean_value + summary_df$sd_value), label = sprintf("p = %.3f", p_value))
+
+
+print(OS833_NFE2L3)
+
+
+
+
+# Calculate the ratios for each gene
+ratio_data <- OS833_STAT1_viability %>%
+  pivot_wider(names_from = drug, values_from = value) %>%  # Use drug for column names and value for data
+  mutate(ratio = cis/ctrl) %>%  # Calculate the ratio
+  select(gene, ratio)     
+
+# Plot the ratios
+ggplot(ratio_data, aes(x=gene, y=ratio, fill=gene)) +
+  geom_bar(stat="identity", position="dodge") +
+  labs(title="Cis/Ctrl Ratio by Gene", 
+       y="Cis/Ctrl Ratio", x="Gene") +
+  theme_bw() +
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank()) +
+  scale_fill_manual(values = pastel_colors)
+
+
+
+
 ######    STAT1   ######
 
 
