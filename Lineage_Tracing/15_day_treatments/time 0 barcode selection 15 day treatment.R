36c75b6f4ffbede678380011f0fb6e98ce5808fd
Brenda Melano <brendamelano@Brendas-iMac.local>
2024-11-13 07:01:43 -0800

    preparing rds files for ciberatac

--- Lineage_Tracing/15_day_treatments/time 0 barcode selection 15 day treatment.R
+++ Lineage_Tracing/15_day_treatments/time 0 barcode selection 15 day treatment.R
@@ -471,4 +471,94 @@ OS384_trajectory_barcodes <- as.data.frame(rc_384_trajectory_barcodes)
 # writing the csv to the single cell folder
 write.csv(OS384_trajectory_barcodes, "~/Desktop/scRNAseq_LT_analysis/OS384_trajectory_LT_barcodes.csv")
 
+##############
+
+
+### Preparing Rds files for mave - need to move to another script
+
+library(Matrix)
+
+# Read the count matrix
+count_matrix <- readMM('/Users/brendamelano/Desktop/OS152_count_matrix.mtx')
+
+# Read cell barcodes and gene names
+cell_barcodes <- readLines('/Users/brendamelano/Desktop/OS152_barcodes.tsv')
+gene_names <- readLines('/Users/brendamelano/Desktop/OS152genes.tsv')
+
+# Assign row and column names
+rownames(count_matrix) <- cell_barcodes
+colnames(count_matrix) <- gene_names
+
+# Save as .rds file
+saveRDS(count_matrix, '/Users/brendamelano/Desktop/Reprogramming_Osteosarcoma/plain_scRNAseq_analysis/Brenda_data_for_Mehran/OS152_gene_barcode_matrix.rds')
+
+# Check the dimensions
+dim(count_matrix)
+
+# View the first few rows and columns
+count_matrix[1:5, 1:5]
+
+# Verify row and column names
+head(rownames(count_matrix))
+head(colnames(count_matrix))
+
+
+## 384 ##
+
+
+
+# Read the count matrix
+count_matrix <- readMM('/Users/brendamelano/Desktop/OS384_count_matrix.mtx')
+
+# Read cell barcodes and gene names
+cell_barcodes <- readLines('/Users/brendamelano/Desktop/OS384_barcodes.tsv')
+gene_names <- readLines('/Users/brendamelano/Desktop/OS384_genes.tsv')
+
+# Assign row and column names
+rownames(count_matrix) <- cell_barcodes
+colnames(count_matrix) <- gene_names
+
+# Save as .rds file
+saveRDS(count_matrix, '/Users/brendamelano/Desktop/Reprogramming_Osteosarcoma/plain_scRNAseq_analysis/Brenda_data_for_Mehran/OS384_gene_barcode_matrix.rds')
+
+# Check the dimensions
+dim(count_matrix)
+
+# View the first few rows and columns
+count_matrix[1:5, 1:5]
+
+# Verify row and column names
+head(rownames(count_matrix))
+head(colnames(count_matrix))
+
+
+
+## 742 ##
+
+
+
+# Read the count matrix
+count_matrix <- readMM('/Users/brendamelano/Desktop/OS742_count_matrix.mtx')
+
+# Read cell barcodes and gene names
+cell_barcodes <- readLines('/Users/brendamelano/Desktop/OS742_barcodes.tsv')
+gene_names <- readLines('/Users/brendamelano/Desktop/OS742_genes.tsv')
+
+# Assign row and column names
+rownames(count_matrix) <- cell_barcodes
+colnames(count_matrix) <- gene_names
+
+# Save as .rds file
+saveRDS(count_matrix, '/Users/brendamelano/Desktop/Reprogramming_Osteosarcoma/plain_scRNAseq_analysis/Brenda_data_for_Mehran/OS742_gene_barcode_matrix.rds')
+
+# Check the dimensions
+dim(count_matrix)
+
+# View the first few rows and columns
+count_matrix[1:5, 1:5]
+
+# Verify row and column names
+head(rownames(count_matrix))
+head(colnames(count_matrix))
+
 
